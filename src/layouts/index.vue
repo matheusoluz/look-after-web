afc5841e25d5659f6a836db980a262ca465203de
MatheusLuz <matheus.luz12@gmail.com>
2019-04-02 01:54:39 -0300

    Alteração de layout e rotas

---
+++ src/layouts/index.vue
@@ -0,0 +1,82 @@
+<template>
+  <q-layout class="bg-light">
+    <q-layout-header>
+      <q-toolbar
+        class="q-py-none"
+        color="teal-11"
+      >
+        <q-btn
+          flat
+          round
+          dense
+          icon="menu"
+          @click="ToogleSidebar"
+          color="pink-12"
+        />
+        <q-toolbar-title
+          shrink
+          class="text-pink-4"
+        >
+          <span>{{$NodePackage.productName}}</span>
+        </q-toolbar-title>
+        <q-toolbar-title />
+      </q-toolbar>
+    </q-layout-header>
+
+    <q-layout-drawer
+      side="left"
+      v-model="leftDrawer.show"
+      :mini="leftDrawer.showMini"
+      :width="200"
+    >
+      <q-scroll-area class="fit">
+        <q-list
+          highlight
+          no-border
+          style="padding: 0"
+        >
+          <q-item to="/products">
+            <q-item-side
+              icon="child_friendly"
+              color="pink-11"
+            />
+            <q-item-main label="Estoque" />
+          </q-item>
+          <q-item to="/login/logout">
+            <q-item-side
+              icon="mdi-logout-variant"
+              color="pink-11"
+            />
+            <q-item-main label="Sair" />
+          </q-item>
+        </q-list>
+      </q-scroll-area>
+    </q-layout-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'LayoutIndex',
+  data () {
+    return {
+      envDev: process.env.DEV,
+      leftDrawer: {
+        show: true,
+        showMini: false
+      }
+    }
+  },
+  methods: {
+    ToogleSidebar () {
+      this.$q.platform.is.mobile
+        ? this.leftDrawer.show = !this.leftDrawer.show
+        : this.leftDrawer.showMini = !this.leftDrawer.showMini
+    }
+  }
+}
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -2,11 +2,16 @@
 const routes = [
   {
     path: '/',
-    component: () => import('layouts/MyLayout.vue'),
+    component: () => import('layouts/index.vue'),
     children: [
       {
         path: '',
         component: () => import('pages/Index.vue'),
+        meta: { rule: 'isEveryone' }
+      },
+      {
+        path: '/products',
+        component: () => import('pages/produtcs/list.vue'),
         meta: { rule: 'isEveryone' }
       }
     ]
